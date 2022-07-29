00027efa58c88c7a0bf51c5ab342efdbd56f7e85
Bilal Güler <guleerbilal@gmail.com>
2022-07-30 00:39:22 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MERHABA ARTIK YIL HESAPLAYAN PROGRAMIMA HOŞ GELDİNİZ
+
+* [**Patika Profil Linkim**](https://app.patika.dev/guleerbilal)
+
+* Bu programım kullanıcıdan alınan yılın artık yıl olup olmadığını hesaplıyor.
+
+## *Kodlarım*
+
+*![birinci sayfa](images/1.PNG)
+
+*![ikinci sayfa](images/2.PNG)
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int yil;
+
+        System.out.print("Yıl Giriniz: ");
+        yil = scan.nextInt();
+
+        if (yil % 4 == 0) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else if ((yil % 100 == 0) && (yil % 400 == 0)) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
+    }
+}
